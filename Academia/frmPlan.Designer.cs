c634035aba76908965d6f603f3e288168cd115ea
Diego <shocron.diego@gmail.com>
2014-08-05 11:57:07 -0300

    Agregue el ABM de Modulos, y empece el ABM de Plan, pero falta agregar los cmb a cada uno de la lista.

---
+++ Academia/frmPlan.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Academia
+{
+    partial class frmPlan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbEspecialidad = new System.Windows.Forms.ComboBox();
+            this.clbDescripcionPlan = new System.Windows.Forms.CheckedListBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbEspecialidad
+            // 
+            this.cmbEspecialidad.FormattingEnabled = true;
+            this.cmbEspecialidad.Location = new System.Drawing.Point(121, 31);
+            this.cmbEspecialidad.Name = "cmbEspecialidad";
+            this.cmbEspecialidad.Size = new System.Drawing.Size(121, 21);
+            this.cmbEspecialidad.TabIndex = 0;
+            // 
+            // clbDescripcionPlan
+            // 
+            this.clbDescripcionPlan.FormattingEnabled = true;
+            this.clbDescripcionPlan.Location = new System.Drawing.Point(28, 73);
+            this.clbDescripcionPlan.Name = "clbDescripcionPlan";
+            this.clbDescripcionPlan.Size = new System.Drawing.Size(489, 259);
+            this.clbDescripcionPlan.TabIndex = 1;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(12, 353);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(140, 40);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar cambios";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(482, 353);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(140, 40);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmPlan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 411);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.clbDescripcionPlan);
+            this.Controls.Add(this.cmbEspecialidad);
+            this.Name = "frmPlan";
+            this.Text = "ABM Plan";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbEspecialidad;
+        private System.Windows.Forms.CheckedListBox clbDescripcionPlan;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
